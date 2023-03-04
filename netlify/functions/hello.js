11cb84e8f06df543e67b01c6baf2492b1776b0b2
iain <65234774+gilesiain@users.noreply.github.com>
2023-03-04 19:58:15 +0000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,26 +1,26 @@
-// hello there!
-// 
-// I'm a serverless function that you can deploy as part of your site.
-// I'll get deployed to AWS Lambda, but you don't need to know that. 
-// You can develop and deploy serverless functions right here as part
-// of your site. Netlify Functions will handle the rest for you.
 const fetch = require('node-fetch')
 
-exports.handler = async event => {
-    const options = {
-  method: 'POST',
-  headers: {Authorization: 'Bearer xau_HYYuCnlGvu8BMIWTOD2eMPiUWLSipIx04', 'Content-Type': 'application/json'},
-  body: '{"page":{"size":15}}'
-};
+const API_ENDPOINT = 'https://cat-fact.herokuapp.com/facts'
 
-fetch('https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/query', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+exports.handler = async (event, context) => {
+  let response
+  try {
+    response = await fetch(API_ENDPOINT)
+    // handle response
+  } catch (err) {
     return {
-        statusCode: 200,
-        body: `Hello ${subject}!`,
+      statusCode: err.statusCode || 500,
+      body: JSON.stringify({
+        error: err.message
+      })
     }
-}
+  }
 
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      data: response
+    })
+  }
+}
 

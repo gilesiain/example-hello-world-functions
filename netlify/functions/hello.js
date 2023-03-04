82ba7334420a814853dc162f963f878afa6e4d2e
iain <65234774+gilesiain@users.noreply.github.com>
2023-03-04 21:57:44 +0000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 
-const API_ENDPOINT = 'https://jsonplaceholder.typicode.com/todos/1'
-
 exports.handler = async (event, context) => {
-  let response
+  const data = {
+    body: '{"page":{"size":15}}'
+  };
+  
   try {
-    response = await axios.get(API_ENDPOINT)
-  } catch (err) {
+    const response = await axios.post('https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/query', data, {
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3hhdGEuaW8iLCJzdWIiOiJ1c3JfZ2ttMDBlcWw2dDdrMWNvcjd0OHJtMW82YzQiLCJleHAiOjE2Nzc5Njc4MjEsImlhdCI6MTY3Nzk2NjkyMSwid29ya3NwYWNlcyI6eyI4OW42NWciOnsicm9sZSI6Im93bmVyIn19fQ.9B5UbrMmIbgAMi49tVa2EvtvOa7yMhfz-fYgVbE_djn5b6x_wHb5eaTbSpGHXkgswhLJ-LZBjx4pO23kWei9Bg',
+    'Content-Type': 'application/json'
+      }
+    });
+    
     return {
-      statusCode: err.statusCode || 500,
-      body: JSON.stringify({
-        error: err.message
-      })
-    }
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: error.message
+    };
   }
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      data: response.data
-    })
-  }
-}
+};

ce65d6e48f52c7e3d0246f42b4af3253b03002c3
iain <65234774+gilesiain@users.noreply.github.com>
2023-03-04 22:13:28 +0000

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   try {
     const response = await axios.post('https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/query', {
       headers: {
-        Authorization: 'Bearer xau_HYYuCnlGvu8BMIWTOD2eMPiUWLSipIx04',
+        Authorization: 'Bearer eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3hhdGEuaW8iLCJzdWIiOiJ1c3JfZ2ttMDBlcWw2dDdrMWNvcjd0OHJtMW82YzQiLCJleHAiOjE2Nzc5Njg4NjIsImlhdCI6MTY3Nzk2Nzk2Miwid29ya3NwYWNlcyI6eyI4OW42NWciOnsicm9sZSI6Im93bmVyIn19fQ.WklRAugGyb1u2eqUd6YAz-vCPN2vAc3z1W9YpttP3_URIxShVkZDh1HwRlEu_0oCJmXg5Vx3tST1V1H6abVJAA',
         'Content-Type': 'application/json'
       },
       body: '{"page":{"size":15}}'

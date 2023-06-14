4563bdadab8864f89197581831e5f351fcd7ebf6
iain <65234774+gilesiain@users.noreply.github.com>
2023-06-14 20:42:47 +0100

    Update hello.js

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -2,16 +2,17 @@ import axios from 'axios'
 
 exports.handler = async (event, context) => {
   
-  const apiKey = process.env.xata_api_key;
-  
-  const options = {
-  headers: {Authorization: `Bearer` + apiKey, 'Content-Type': 'application/json'},
-  body: '{"name":"string","email":"a@b.com","bio":"longer text"}'
-};
+  var apiKey = 'Bearer ' + process.env.xata_api_key;
   
   try {
-    const response = await axios.post('https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/data?columns=id',options);
-    
+   const response = await axios({
+    method: 'put',
+    url: 'https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/data?columns=id',
+    data: {
+        headers: {Authorization: apiKey, 'Content-Type': 'application/json'},
+      body: '{"name":"string","email":"a@b.com","bio":"longer text"}'
+    }
+});
     return {
       statusCode: 200,
       body: JSON.stringify(response.data)

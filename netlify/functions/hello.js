f416a18f642ae178639a417728765386f037d5ef
iain <65234774+gilesiain@users.noreply.github.com>
2023-06-14 21:11:57 +0100

    try put test xata

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,12 +1,33 @@
-var api = 'Bearer ' + process.env.xata_api_key
-    const options = {
-  method: 'POST',
-  headers: {Authorization: api, 'Content-Type': 'application/json'},
-  body: '{"name":"string","email":"a@b.com","bio":"longer text"}'
-};
+import axios from 'axios'
 
-fetch('https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/data?columns=id', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
-</script>
+exports.handler = async (event, context) => {
+
+  const apiKey = process.env.xata_api_key;
+
+  try {
+    const response = await axios.post(
+      'https://iain-giles-s-workspace-89n65g.eu-west-1.xata.sh/db/deadly:main/tables/Users/data?columns=id',
+      {
+          name: 'test',
+          email: 'a@b.com',
+          bio: 'longer text'
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${apiKey}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    );
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.response.data)
+    };
+  }
+};
